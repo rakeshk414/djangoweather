c264d6743ea8d6836c2742e908a31e26551cf87f
Rakesh <rakesh@testing.com>
2025-09-05 23:22:40 +0530

    added CSS

--- lookup/views.py
+++ lookup/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 
 
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request = requests.get("https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=89129&date=2025-09-05&distance=10&API_KEY=72A00659-85BF-4F88-B8A2-08FBDC90795E")
+	
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api="Error..."
+	return render(request, 'home.html', {'api': api})
 
 def about(request):
 	return render(request, 'about.html', {})
